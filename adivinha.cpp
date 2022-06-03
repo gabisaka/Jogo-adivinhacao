c0b23ab97da81a4cb3db33e27d269753548394f5
gabisaka <gabrielayts@hotmail.com>
2022-06-03 08:27:44 -0400

    Adivinhacao

---
+++ adivinha.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+int main () {
+    std::cout << "*************************************";
+    std::cout << "* Bem-vindos ao jogo da adivinhação *";
+    std::cout << "*************************************";
+}
+
