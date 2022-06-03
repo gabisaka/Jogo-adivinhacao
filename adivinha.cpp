41166d6ce181e97ca86a5b270caa9a76c091ef34
gabisaka <gabrielayts@hotmail.com>
2022-06-03 08:48:18 -0400

    Adicionando o número secreto

--- adivinha.cpp
+++ adivinha.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
+using namespace std;
 
 int main () {
-    std::cout << "*************************************";
-    std::cout << "* Bem-vindos ao jogo da adivinhação *";
-    std::cout << "*************************************";
+    cout << "*************************************" << endl;
+    cout << "* Bem-vindos ao jogo da adivinhação *" << endl;
+    cout << "*************************************" << endl;
+
+    int numero_secreto = 42;
+    cout << "O número secreto é " << numero_secreto <<". Não conte para ninguém!" << endl; 
 }
 
